296683bebcf2584741328d2edf7f07f5f30de673
Emilce Robles <emi.nrobles@gmail.com>
2023-07-03 00:11:45 -0300

    Actualizacion funciones

---
+++ FUNCIONES/5-sumayresta.py
@@ -0,0 +1,9 @@
+def sumayresta(a, b):
+    suma = a + b
+    resta = a - b
+    print("Suma:", suma)
+    print("Resta:", resta)
+
+
+# Llamada de la funcion y asignacion de 2 valores ejemplo
+sumayresta(20, 5)
